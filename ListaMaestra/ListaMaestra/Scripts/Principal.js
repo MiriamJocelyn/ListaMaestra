4412b95025dbc8f06964acd8c65bb2be9277831f
MiriamJocelyn <mirismarquez@gmail.com>
2022-12-05 12:21:11 -0600

    Inicio del proyecto

---
+++ ListaMaestra/ListaMaestra/Scripts/Principal.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    prueba()
+})
+
+function prueba() {
+
+    var htmlTbody = '<tr>'
+    htmlTbody +='<td><input type="checkbox" /></td>'
+    htmlTbody += '<td></td>'
+
+    htmlTbody += '</tr>'
+
+    $("#tbodyPrincipal").append(htmlTbody)
+
+    //console.log('orueba');
+}
